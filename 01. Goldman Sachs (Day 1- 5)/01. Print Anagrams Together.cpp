e227b824033f296c6f8f96c2d8231bd64b0209c2
ArvinderSingh <ssingharvinder@outlook.com>
2022-01-02 11:29:16 +0530

    update

---
+++ 01. Goldman Sachs (Day 1- 5)/01. Print Anagrams Together.cpp
@@ -0,0 +1,33 @@
+/*
+LINK : https://practice.geeksforgeeks.org/problems/print-anagrams-together/1/
+Leetcode :  https://leetcode.com/problems/group-anagrams
+*/
+
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& input) {
+        
+        vector<vector<string>> ans;
+        unordered_map<string,int> mapping;
+        //maps key (to sorted string) to group index
+        // this will also solve ordering problem
+        
+        for(string cur:input){
+            
+            //generate key for group
+            string key = cur;
+            sort(key.begin(),key.end());
+            
+            // see if group already exist, if not create one
+            if(mapping.find(key) == mapping.end()){
+                mapping[key] = ans.size();
+                ans.push_back(vector<string>());
+            }
+            
+            //assign to correct group
+            ans[mapping[key]].push_back(cur);
+        }
+        
+        return ans;
+    }
+};
